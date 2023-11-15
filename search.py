92a8e0dcfb894621380db336a1e0bd25e600dd13
rosbourne <rosbourne@wesleyan.edu>
2023-11-15 10:00:31 -0500

    Module to provide backend support for user search

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 # Module to provide backend support to parse through database
 # and find 3-5 most related resources. 
-
+import resource_model
 import nltk
 from nltk.corpus import wordnet
 nltk.download('omw-1.4')
@@ -11,8 +11,7 @@ low_priority_keywords = []
 
 def filter():
     """Collects user input and search the database for closest match.
-    The search works by:
-    1. Finding matches between values the user enters in "explicit 
+    The search works by Finding matches between values the user enters in "explicit 
     fields" (e.g Category) and values found int the database. This method help 
     in filtering by explicit values.
     
@@ -29,14 +28,17 @@ def refine_keywords():
     """
     return "refined_list"
 
-def calculate_similarity(filter_results):
+def calculate_similarity(filter_results, keywords):
     """Calculate the scoring of each resource model based on synonym
     match with keywords from the user's description. Similarity is 
     calculated based on number of synonyms shared by a resource db model 
     and the each keyword. 
 
     Args:
-        search_results (_type_): _description_
+        filter_results (list): preliminary list of resource models that match
+        explicit fields.
+        keywords (list): keywords whose synonyms will be used to compare
+        agaisnt each filter_result model in the database.
 
     Returns:
         dict: a dict with keys the similairy score and value the resource
@@ -55,5 +57,14 @@ def complete_search(resource_similarity):
         html: rendered html template with list of resources
     """
     return "render_template(search_results.html, results=results)"
+
+
+if __name__ == "__main__":
+    complete_search(calculate_similarity(filter(), refine_keywords()))
+
+    #UNIT TESTS - SEARCH FEATURE 
+    # Testing filter() 
+        #Initialize a list of ResourceModels 
+        
 
 
