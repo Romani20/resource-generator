b25afb627af18af1a056e5215a1328621b90e8c5
Melat <mgebremeskel@wesleyan.edu>
2023-11-24 16:23:36 -0500

    This is just like the sign up page, it's so we can autheticate users

--- resource-generator/src/sign-In-page.js
+++ resource-generator/src/sign-In-page.js
@@ -1,14 +1,56 @@
-import React from 'react';
+// import React from 'react';
 import signInCss from './sign-In.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import CreateAccount from './create_account_page.js';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
+const AuthContext = React.createContext();
 
-class SignInPage extends React.Component {
-    //This is a page the user will see when they open Resource genrator for the first time.
-    //It'll ask them to create an account if they don't have one or to login if they already do. 
-    //The code below shows a simple/plain implementation of this
-    render(){
+const AuthProvider = ({ children }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const login = () => {
+        // Perform login logic (e.g., send a request to the Flask backend)
+        // Update isAuthenticated state accordingly
+        setIsAuthenticated(true);
+    };
+
+    const logout = () => {
+        // Perform logout logic (e.g., send a request to the Flask backend)
+        // Update isAuthenticated state accordingly
+        setIsAuthenticated(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+const SignInPage = () => {
+    const { login } = useAuth();
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+    const history = useHistory();
+
+    const handleInputChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSignIn = () => {
+        // Send a request to the Flask backend to check credentials and log in
+        // Update the login function to communicate with your backend
+        login();
+        history.push('/main'); // Redirect to the main page after successful login
+    };{
         return (
           <Router>
             <div className="SignInPage">
