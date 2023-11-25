b3033ac4f9e47cd01e5b2fbf59165882db8362f6
rosbourne <rosbourne@wesleyan.edu>
2023-11-25 14:14:47 -0500

    Method to search resource table and return appropriate resource

--- website/views.py
+++ website/views.py
@@ -1,8 +1,6 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
-from . import db
-import json
+from .models import Resource
 
 views = Blueprint('views', __name__)
 
@@ -10,28 +8,27 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the html
+    if request.method == 'POST':
+        category = request.form.get("category")
+        q = request.form.get("q")
+    
+        if q and category:
+            return redirect(url_for('views.search_results', category=category, q=q))
 
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
-
-    return render_template("home.html", user=current_user)
+    return render_template('home.html', user=current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
+@views.route('/search_results', methods=['GET'])
+def search_results():
+    category = request.args.get('category')
+    keyword = request.args.get('q')
 
-    return jsonify({})
+    if category and keyword:
+        results = Resource.query.filter(
+            Resource.resource_type.ilike(f"%{category}%") &
+            (Resource.resource_name.ilike(f"%{keyword}%") | Resource.resource_type.ilike(f"%{keyword}%"))
+        ).all()
+    else:
+        results = []
+
+    return render_template('search.html', results=results, user=current_user)
