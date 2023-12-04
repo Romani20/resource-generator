5c34f255b64a4938c8689ff88bfb456f31042a5c
rosbourne <rosbourne@wesleyan.edu>
2023-12-03 19:23:18 -0500

    Finding a standard value for similarity cut-off

---
+++ nlp_threshold.py
@@ -0,0 +1,41 @@
+import spacy
+from spacy_wordnet.wordnet_annotator import WordnetAnnotator
+from wonderwords import RandomWord
+import csv
+
+#spacy.cli.download("en_core_web_md")
+nlp = spacy.load("en_core_web_md")
+
+with open("edited_synonyms.csv", 'r') as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)
+
+            total_sim, avg = 0, 0
+            for row in csv_reader:
+                  w1, w2 = row[0], row[1]
+                  print(w1, w2)
+                 
+                  w1 = nlp.vocab[w1]
+                  w2 = nlp.vocab[w2]
+                  total_sim += w2.similarity(w1)
+                  avg = round(total_sim/399, 3)
+            print(avg)
+
+with open("edited_antonyms.csv", 'r') as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)
+
+            total_sim, avg = 0, 0
+            for row in csv_reader:
+
+                  w1, w2 = row[0], row[1]
+                  print(w1, w2)
+                 
+                  w1 = nlp.vocab[w1]
+                  w2 = nlp.vocab[w2]
+                  total_sim += w2.similarity(w1)
+                  avg = round(total_sim/399, 3)
+            print(avg)            
+            
+
+
