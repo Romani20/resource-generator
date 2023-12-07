774e394766aea67e744a8fddcfc3608290ee75a7
rosbourne <rosbourne@wesleyan.edu>
2023-12-07 01:44:24 -0500

    Modified method to find resource based on similarity

--- search.py
+++ search.py
@@ -1,23 +1,14 @@
-# Module to provide backend support to parse through database
-# and find 3-5 most related resources. 
-import nltk
-from nltk.corpus import wordnet
-nltk.download('omw-1.4')
-# import views
-from flask import Flask, jsonify
-from flask_login import login_required, current_user
-from flask_sqlalchemy import SQLAlchemy
 from unit_tests import genericUnitTest
 import spacy
-import csv, json
+import json
 from website import models 
 
 nlp = spacy.load("en_core_web_md")
 
 # Keywords that will be removed from users description, if available, 
-# to optimize search
+# to optimize search. Wprds are generated from nlk module
 low_priority_keywords = ['can', 'has', 'if', 've', "don't", 'weren', 'we', 'ain', 'above',
-                          'at', 'each', 'll', "that'll", 'is', 'other', "weren't", 'isn',
+                          'at', 'each', 'll', "that", 'is', 'other', "weren't", 'isn',
                         'doing', "wouldn't", 'couldn', 'shouldn', "mustn't", 'i', 'this',
                         'both', 'by', 'own', 'aren', 'for', 'once', 'not', "aren't", 'having',
                         'am', 'its', "should've", 'now', 'out', 'in', "shan't", 'why', "should"
@@ -54,39 +45,56 @@ def convert_description_to_array(description):
         if i.lower() not in (item.lower() for item in low_priority_keywords):
             refined_desc.append(i)
 
-    #return json.dumps(refined_desc.split())
     return json.dumps(refined_desc)
 
-def find_resource_by_keyword_similarity(filter_results, user_input):
+
+def remove_low_priority_keywords(user_description):
+    """_summary_
+
+    Args:
+        user_description (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    new_list = []
+    for i in user_description:
+        if i.lower() not in (item.lower() for item in low_priority_keywords):
+            new_list.append(i)
+
+    return new_list
+
+def find_resource_by_keyword_similarity(results, keywords):
     """Calculate the scoring of each resource model based on synonym
     match with keywords from the user's description. Similarity is 
     calculated based on number of synonyms shared by a resource db model 
     and the each keyword. 
 
     Args:
-        filter_results (list): preliminary list of resource models that match
-        explicit fields.
+        filter_results (list): preliminary list of resource models that has
+        explict category match.
         keywords (list): keywords whose synonyms will be used to compare
         agaisnt each filter_result model in the database.
 
     Returns:
         dict: a dict with keys the similairy score and value the resource
     """
-    found_resources = set()
-    if filter_results != []:
-        for result in filter_results:
-            keywords = result.keywords
-            for processed_keyword in [nlp.vocab[keyword] for keyword in keywords]:
-                for i in (convert_description_to_array(user_input)):
-                    similarity_score = processed_keyword.similarity(nlp.vocab[i])
-                    if similarity_score >= 0.30:
-                        found_resources.add(result)
-    return found_resources
+    filtered_results = set()
+    if results:
+            for result in results:
+                keywords = result.keywords
+                for processed_keyword in [nlp.vocab[keyword] for keyword in keywords]:
+                    for i in keywords:
+                        similarity_score = processed_keyword.similarity(nlp.vocab[i])
+                        if similarity_score >= 0.3:
+                            filtered_results.add(result)
+        
+    return filtered_results
 
 if __name__ == "__main__":
     # UNIT TESTS - SEARCH FEATURE 
     #testing convert_description_to_array()
-    res = '["food"]'
+    res = ["food"]
     genericUnitTest(convert_description_to_array, ('i want food',), (res,))
 
     print("\n")
