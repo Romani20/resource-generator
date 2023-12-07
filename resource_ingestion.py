98954f17301e29e10823cd31ee511a68df7736b3
rosbourne <rosbourne@wesleyan.edu>
2023-12-07 01:45:06 -0500

    Removed redundant imports

--- resource_ingestion.py
+++ resource_ingestion.py
@@ -1,17 +1,9 @@
 import pandas as pd
 from website import db, models
-# import models
-# from __init__ import db
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import csv, json
-from sqlalchemy import and_
+import csv
 import search 
-
-# Resources and their metadata are ingested from two sources:
-# primary - manually created json objects holding resource information
-# secondary - user added data 
-# This module will ingest from a csv into database.
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
@@ -36,40 +28,10 @@ def manually_write_resources_to_database():
             next(csv_reader)
 
             for row in csv_reader:
-                keywords_json = convert_description_to_array(row[2])
+                keywords_json = search.convert_description_to_array(row[2])
                 resource = models.Resource(id = hash(row[0]), resource_name=row[0], resource_type=row[1],
                                     link_to_website=row[3], keywords=keywords_json, email=row[4])
                     
                 db.session.add(resource)
                 db.session.commit()
                 db.session.close()
-
-def convert_description_to_array(description):
-    """_summary_
-
-    Args:
-        description (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    refined_desc = []
-    keywords_list = description.split(" ")
-
-    for i in keywords_list:
-        if i.lower() not in (item.lower() for item in search.low_priority_keywords):
-            refined_desc.append(i)
-
-    return json.dumps(refined_desc)
-
-def write_user_inputted_resource_to_database():
-    """Parses through the metadata the user enrers for a new resource to be added
-       to the database and add the resource to the db. 
-
-    Returns:
-        None_: new resource is added to the database.
-    """
-    return "user_inputted"
-
-
-manually_write_resources_to_database()
